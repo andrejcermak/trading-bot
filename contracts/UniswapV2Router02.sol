69a369e76aa809d490e149c7f8afaf1b2702b4aa
cermak <andrej.cermak.1999@gmail.com>
2022-03-11 14:34:18 +0100

    Removing hanging part of code

--- contracts/UniswapV2Router02.sol
+++ contracts/UniswapV2Router02.sol
@@ -9,9 +9,6 @@ interface IUniswapV2Router01 {
     function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
         external
         returns (uint[] memory amounts);
-        external
-        payable
-        returns (uint[] memory amounts);
     function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
 }
 
